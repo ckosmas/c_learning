841739fa8a7737e791e7750774ad2b483cab8e22
Chris Kosmas <chris@ckosmas.com>
2026-03-12 14:16:20 -0500

    Create for_loops.c

---
+++ notes/for_loops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main (){
+    
+    for(int i = 10; i >= 0; i--){
+        Sleep(1000);
+        printf("%d\n", i);
+
+    }
+
+    printf("Happy New Year!");
+
+    return 0;
+}
