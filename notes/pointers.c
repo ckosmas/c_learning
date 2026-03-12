caf78b9b970d4547d35219728b8c511384989408
Chris Kosmas <chris@ckosmas.com>
2026-03-12 14:24:29 -0500

    Create pointers.c

---
+++ notes/pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void birthday(int* age);
+
+int main(){
+
+    int age = 25;
+    int *pAge = &age;
+
+    birthday(pAge);
+    printf("You are %d years old", age);
+
+    return 0;
+}
+void birthday(int* age){
+    //pass by reference
+    (*age)++;
+}
