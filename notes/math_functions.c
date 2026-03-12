300cf64ed98d408a35d73de98db10feb0ed426f1
Chris Kosmas <chris@ckosmas.com>
2026-03-12 14:08:29 -0500

    Create math_functions.c

---
+++ notes/math_functions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    
+    float x = 45;
+    //x = sqrt(x); square root
+    //x = pow(x,2); exponents
+    //x = round(x); round
+    //x = ceil(x); round up 
+    //x = floor(x); round down
+    //x = abs(x); distance from zero as round number
+    //x = log(x); natural logrhythm
+    //x = sin(x); trignometry functions example
+    //x = cos(x); trignometry functions example
+    //x = tan(x); trignometry functions example
+    
+    printf("%f", x);
+
+
+    return 0;
+}
